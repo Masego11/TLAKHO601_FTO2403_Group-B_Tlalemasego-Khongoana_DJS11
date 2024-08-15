bd4b5d3bf645a3fa151a696e07a25497d286f94b
Tlalemasego <tlalemasego@gmail.com>
2024-08-15 10:15:54 +0200

    git commit -  fetched podcasts from Api and used the usepodcastsstore to fetch and display the podcasts

--- src/pages/podcasts.jsx
+++ src/pages/podcasts.jsx
@@ -1,7 +1,33 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import useProductsStore from "../zustand/store";
+
 function Podcasts() {
-    return(
-       <h1>Find all podcasts here</h1>
-    )
+    const { podcasts, fetchPodcasts, error } = useProductsStore();
+
+    useEffect(() => {
+        if(!podcasts) {
+            console.log("Fetching podcasts");
+            fetchPodcasts();
+        }
+    }, [podcasts, fetchPodcasts]);
+        if(error) {
+            return <h1>An Error occured while loading podcasts: {error}</h1>;
+    
+}
+
+return (
+    <div>
+        <h1>Podcasts</h1>
+        <ul>
+            {podcasts && podcasts.map((podcast) => (
+            <li key={podcast.id}>
+                <Link to={`/podcasts/${podcast.id}`}>{podcast.title}</Link>
+            </li>
+            ))}
+        </ul>
+    </div>
+);
 }
 
 export default Podcasts
