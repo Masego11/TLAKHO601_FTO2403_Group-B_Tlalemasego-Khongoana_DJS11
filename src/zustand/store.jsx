f18634a0675981223d90166735cd77312d3d58ad
Tlalemasego <tlalemasego@gmail.com>
2024-08-21 10:31:56 +0200

    git commit - changed the name of my fetchGenre function.

--- src/zustand/store.jsx
+++ src/zustand/store.jsx
@@ -12,6 +12,7 @@ const usePodcastsStore = create((set, get) => ({
     fetchPodcasts: async() => {
         try{
             let podcasts = JSON.parse(localStorage.getItem('podcasts'));
+
             if(!podcasts || podcasts.length === 0) {
                 const response = await fetch ('https://podcast-api.netlify.app');
                 podcasts = await response.json();
@@ -26,27 +27,28 @@ const usePodcastsStore = create((set, get) => ({
             console.error("error fetching podcasts", error);
         }
     },
-    //Fetch genre details by ID 
-    fetchGenre: async(id) => {
-        try{
-            let genres = JSON.parse(localStorage.getItem('genres'));
-            const genre = genres.find(g => g.id === id);
 
-            if(!genre) {
-                const response = await fetch (`https://podcast-api.netlify.app/genre/${id}`);
-                const genreData = await response.json();
-                genres.push(genreData);
-    
-                genres.sort((a, b) => a.title.localeCompare(b.title));
-                localStorage.setItem("genres", JSON.stringify(genres));
-                set({ genres }) 
-            }
-        } catch (error) {
-            //If there's an error set error
-            set({ error: "Failed to fetch genres" });
-            console.error("error fetching genres", error);
+  //Fetch genre details by ID 
+  fetchGenresById: async(id) => {
+    try{
+        let genres = JSON.parse(localStorage.getItem('genres'));
+        const genre = genres.find(g => g.id === id);
+
+        if(!genre) {
+            const response = await fetch (`https://podcast-api.netlify.app/genre/${id}`);
+            const genreData = await response.json();
+            genres.push(genreData);
+
+            genres.sort((a, b) => a.title.localeCompare(b.title));
+            localStorage.setItem("genres", JSON.stringify(genres));
+            set({ genres }) 
         }
-    },
+    } catch (error) {
+        //If there's an error set error
+        set({ error: "Failed to fetch genres" });
+        console.error("error fetching genres", error);
+    }
+},
     
     // back buttons
    
