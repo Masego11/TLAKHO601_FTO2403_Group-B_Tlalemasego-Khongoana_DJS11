39d0581e44fddbf8a394aa8b8b111a8d4dd485d6
Tlalemasego <tlalemasego@gmail.com>
2024-08-22 12:34:45 +0200

    git commit - made changes to routes structure.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import GenreDetails from "./pages/preview/genredetails";
 import PreviewLayout from "./components /previewlayout";
 import Genres from "./pages/preview/podcastgenres";
 import PodcastDetails from "./pages/podcasts /podcastdetails";
+import Episodes from "./pages/podcasts /episodedetails";
 import "./index.css"
 
 
@@ -15,20 +16,20 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="podcasts" element={<Podcasts />} />
-        <Route path="podcasts/:id" element={<PodcastDetails />} />
-
-        <Route path="preview" element={<PreviewLayout />}>
+          <Route index element={<Home />} />
+          <Route path="podcasts" element={<Podcasts />} />
+          <Route path="podcasts/:id" element={<PodcastDetails />} />
+          {/* Updated route to handle episode navigation properly */}
+          <Route path="podcasts/:id/seasons/:seasonId/episodes" element={<Episodes />} />
+          <Route path="about" element={<About />} />
+          <Route path="preview" element={<PreviewLayout />}>
             <Route index element={<Genres />} />
             <Route path="genres/:id" element={<GenreDetails />} /> 
-        </Route>
+          </Route>
         </Route>
       </Routes>
-    
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
