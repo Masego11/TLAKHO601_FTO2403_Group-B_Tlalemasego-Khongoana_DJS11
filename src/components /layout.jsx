e590278de6f3b0b9ab8bab1fb9d0658c7187d07d
Tlalemasego <tlalemasego@gmail.com>
2024-08-23 12:16:42 +0200

    git commit - added audio player display to my layout to allow display of audio being played.

--- src/components /layout.jsx
+++ src/components /layout.jsx
@@ -1,21 +1,25 @@
 //The layout component provides a wrapper for the  application's pages. 
 // Imports
 import { Outlet } from "react-router-dom";
-import Header from "./header";
-import Footer from "./footer";
+import Header from "./Header";
+import Footer from "./Footer";
+import AudioPlayer from "./AudioPlayer";
+import usePodcastsStore from "../zustand/store";
 
-// Structure of components 
 function Layout() {
+    const { currentEpisode } = usePodcastsStore();
+
     return (
         <div className="site-wrapper">
             <Header />
+            {currentEpisode && <AudioPlayer />}
             <main>
                 <Outlet />
             </main>
+            
             <Footer />
         </div>
-    )
+    );
 }
 
-// Exporting Layout Component
-export default Layout
+export default Layout;
