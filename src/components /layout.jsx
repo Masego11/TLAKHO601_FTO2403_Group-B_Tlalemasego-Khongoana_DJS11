eccb8ada0c800237869704491be8e4e219944422
Tlalemasego <tlalemasego@gmail.com>
2024-08-26 15:49:08 +0200

    git commit  - fixed deployment issue with header file path

--- src/components /layout.jsx
+++ src/components /layout.jsx
@@ -1,8 +1,8 @@
 //The layout component provides a wrapper for the  application's pages. 
 // Imports
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./header";
+import Footer from "./footer";
 import AudioPlayer from "./AudioPlayer";
 
 function Layout() {
